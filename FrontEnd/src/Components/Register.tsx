bd1e169101d4087b08fca1cfbbad71d544c4e6ce
John <keokline2015@gmail.com>
2024-02-05 00:34:03 -0600

    Register page styles

--- FrontEnd/src/Components/Register.tsx
+++ FrontEnd/src/Components/Register.tsx
@@ -46,54 +46,90 @@ const Register = (props: { setChangePage: Function }) => {
 
   return (
     <>
-      {error && <h1>{error}</h1>}
-      <h1>Register</h1>
-      <form id="form">
-        <input
-          autoComplete="off"
-          required
-          type="text"
-          id="name"
-          placeholder="Username"
-          value={user.name}
-          onChange={handleChange}
-        />
-        <input
-          autoComplete="off"
-          required
-          type="text"
-          id="email"
-          placeholder="Email"
-          value={user.email}
-          onChange={handleChange}
-        />
-        <input
-          required
-          type="password"
-          id="pwd"
-          placeholder="Password"
-          value={user.pwd}
-          onChange={handleChange}
-        />
-        <input
-          required
-          type="password"
-          id="pwd2"
-          placeholder="Confirm Password"
-          value={user.pwd2}
-          onChange={handleChange}
-        />
-        <button onClick={handleSubmit}>Register</button>
-      </form>
-      <p>Already Have An Account?</p>
-      <button
-        onClick={(e: React.FormEvent) => {
-          e.preventDefault();
-          props.setChangePage("login");
-        }}
-      >
-        Sign In!
-      </button>
+      <div className="w-screen h-screen grid grid-cols-1 items-center justify-items-center">
+        <form
+          className={`bg-stone-300 w-3/6 h-4/6 rounded-3xl items-center justify-items-center grid`}
+          id="form"
+        >
+          {error && <h1>{error}</h1>}
+          <h1 className="text-3xl">Register</h1>
+          <div className={`grid`}>
+            <label className={`pl-5 text-2xl`} htmlFor="name">
+              Username
+            </label>
+            <input
+              className="rounded-3xl text-left p-3 px-6"
+              autoComplete="off"
+              required
+              type="text"
+              id="name"
+              placeholder="Example Username"
+              value={user.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`grid`}>
+            <label className={`pl-5 text-2xl`} htmlFor="name">
+              Email
+            </label>
+            <input
+              className="rounded-3xl text-left p-3 px-6"
+              autoComplete="off"
+              required
+              type="text"
+              id="email"
+              placeholder="example@example.com"
+              value={user.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`grid`}>
+            <label className={`pl-5 text-2xl`} htmlFor="name">
+              Password
+            </label>
+            <input
+              className="rounded-3xl text-left p-3 px-6"
+              required
+              type="password"
+              id="pwd"
+              placeholder="Password"
+              value={user.pwd}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={`grid`}>
+            <label className={`pl-5 text-2xl`} htmlFor="name">
+              Varify Password
+            </label>
+            <input
+              className="rounded-3xl text-left p-3 px-6"
+              required
+              type="password"
+              id="pwd2"
+              placeholder="Confirm Password"
+              value={user.pwd2}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className={`bg-blue-500 text-2xl p-3 px-16 rounded-3xl`}
+            onClick={handleSubmit}
+          >
+            Register
+          </button>
+          <div>
+            <p>Already Have An Account?</p>
+            <button
+              onClick={(e: React.FormEvent) => {
+                e.preventDefault();
+                props.setChangePage("login");
+              }}
+            >
+              Sign In!
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
