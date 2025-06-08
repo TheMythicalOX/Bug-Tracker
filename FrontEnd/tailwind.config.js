8b3d83ebab39f5b6b27b4b65e30b0ae96b5a29f7
TheMythicalOX <keokline2015@gmail.com>
2025-06-08 00:27:23 -0500

    little more

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,ts,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                "pritext": "#4754b2",
-                "back": "#ffdce2",
-                "darkback": "#240006",
-                "primary": "#ba59b5",
-                "secondary": "#87588c",
-                "accent": "#CB429F"
-            }
-        }
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pritext: "#e8f1f2",
+        back: "#CCCCCC",
+        darkback: "#001a23",
+        primary: "#31493C",
+        secondary: "#7A9E7E",
+        accent: "#B3EFB2",
+      },
     },
-    plugins: []
+  },
+  plugins: [],
 };
--- PHP/config.php
+++ PHP/config.php
@@ -4,14 +4,14 @@ ini_set("session.use_only_cookies", 1);
 ini_set("session.use_strict_mode", 0); # change to 1 when using https
 ini_set("session.cookie_samesite", "Lax");
 ini_set("sessions.save_path", "/tmp");
-header("Access-Control-Allow-Origin: http://172.22.20.64:3000");
+header("Access-Control-Allow-Origin: http://192.168.4.130:3000");
 header('Access-Control-Allow-Headers: Content-Type');
 header("Access-Control-Allow-Credentials: true");
 
 
 session_set_cookie_params([
     "lifetime" => 3600,
-    "domain" => "172.22.20.64",
+    "domain" => "192.168.4.130",
     "path" => "/",
     "secure" => false, # change to true when using https
     "httponly" => true,
